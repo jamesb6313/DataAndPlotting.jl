bd54a522d3b8f56d8e39a56cf8c8b30bd68790b0
jamesb6313 <38964563+jamesb6313@users.noreply.github.com>
2018-09-29 22:56:47 -0300

    Appveyor fails because no tmp directory is uploaded
    Appveyor fails because no tmp directory is uploaded
    put file array.h5 into test directory

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,12 +5,14 @@ filename ="array.h5"
 arr = init()
 
 #will not overwrite existing file, therefore remove file first
-rm("C:/Julia/DataAndPlotting.jl/tmp/array.h5", force=true)
+# should access file in current directory which is test - 
+# therfore file would be C:/Julia/DataAndPlotting.jl/test/array.h5
+rm("array.h5", force=true) 
 
-output(arr, "C:/Julia/DataAndPlotting.jl/tmp/array.h5")
+output(arr, "array.h5")
 # for windows try - output(arr, "/temp/array.h5")
 
-arr2 = input("C:/Julia/DataAndPlotting.jl/tmp/array.h5")
+arr2 = input("array.h5")
 @test isequal(arr2, arr)
 
 plotname = "wave.png"		#gets put in current directory "C:/Julia/DataAndPlotting.jl/test"
